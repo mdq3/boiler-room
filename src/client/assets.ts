ad04ef9003a48bf7a24a997005cb7402519315bf
Michael Quested <1479991+mdq3@users.noreply.github.com>
2023-02-05 10:15:14 +0000

    Remove font awesome

--- src/client/assets.ts
+++ src/client/assets.ts
@@ -1,4 +1,3 @@
-import "@fortawesome/fontawesome-free/js/all.min.js";
 import "./assets/scss/main.scss";
 import "file-loader?name=index.html!./assets/index.html";
 import "file-loader?name=favicon.ico!./assets/favicon.ico";
